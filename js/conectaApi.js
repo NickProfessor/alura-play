d7661b30b998f02a1d7378ba952aef7be9ed0b28
Nickollas <nickollassilva90@gmail.com>
2024-04-14 21:57:39 -0300

    Criando a função de adicionar videos através do metodo POST

---
+++ js/conectaApi.js
@@ -0,0 +1,28 @@
+async function listaVideos() {
+    const conexao = await fetch("http://localhost:3000/videos");
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+
+async function criarVideo(titulo, descricao, url, imagem){
+    const conexao = await fetch("http://localhost:3000/videos", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify({
+            titulo: titulo,
+            descricao: `${descricao} mil vizualizações`,
+            url: url,
+            imagem: imagem
+        })
+    });
+    const conexaoConvertida = await conexao.json();
+
+    return conexaoConvertida;
+}
+export const conectaApi = {
+    listaVideos,
+    criarVideo
+}
