88e72963838dd9a6ec1895c21f465acf8da18cc2
Nickollas <nickollassilva90@gmail.com>
2024-04-16 09:31:33 -0300

    finalizando o projeto

--- js/conectaApi.js
+++ js/conectaApi.js
@@ -5,24 +5,36 @@ async function listaVideos() {
     return conexaoConvertida;
 }
 
-async function criarVideo(titulo, descricao, url, imagem){
+async function criaVideo(titulo, descricao, url, imagem) {
     const conexao = await fetch("http://localhost:3000/videos", {
         method: "POST",
         headers: {
-            "Content-Type": "application/json"
+            "Content-type": "application/json"
         },
         body: JSON.stringify({
             titulo: titulo,
-            descricao: `${descricao} mil vizualizações`,
+            descricao: `${descricao} mil visualizações`,
             url: url,
             imagem: imagem
         })
     });
+    if (!conexao.ok) {
+        throw new Error("Não foi possível enviar o vídeo")
+    }
+    const conexaoConvertida = conexao.json();
+
+    return conexaoConvertida;
+}
+
+async function buscaVideo(termoDeBusca) {
+    const conexao = await fetch(`http://localhost:3000/videos?q=${termoDeBusca}`);
     const conexaoConvertida = await conexao.json();
 
     return conexaoConvertida;
 }
+
 export const conectaApi = {
     listaVideos,
-    criarVideo
+    criaVideo,
+    buscaVideo
 }
